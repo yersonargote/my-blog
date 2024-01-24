0bc2c0f0c2861e5badd3b26dd8edea701bf34b3f
yersonargote <yersonargotev@unicauca.edu.co>
2024-01-23 23:16:01 -0500

    Add new terminal guides and update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,41 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: "Coding",
+			favicon: "./favicon.svg",
+			logo: {
+				src: "./src/assets/logo.webp",
+				light: "./src/assets/logo.webp",
+				dark: "./src/assets/logo.webp",
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: "https://github.com/yersonargote/my-blog",
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: "Terminales",
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
+						{ label: "Windows", link: "/terminales/windows" },
+						{ label: "Ubuntu/Linux", link: "/terminales/linux" },
+						{ label: "IOS", link: "/terminales/ios" },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Reference",
+					autogenerate: { directory: "reference" },
 				},
 			],
+			locales: {
+				root: {
+					label: "Español",
+					lang: "es",
+				},
+			},
 		}),
 	],
 });
